1f5a8a982cf581bdf79f0fa0321c0634493e2236
daksh03 <dakshchoudhary.1234@gmail.com>
2026-08-05 20:56:31 +0530

    First comitt for AI project

---
+++ src/main/java/com/spring/ai/firstproject/first_project/controller/ChatController.java
@@ -0,0 +1,28 @@
+package com.spring.ai.firstproject.first_project.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ChatController {
+	
+	private ChatClient chatClient;
+	
+	private ChatController(ChatClient.Builder builder) {
+		this.chatClient=builder.build();
+	}
+
+	@GetMapping("/chat")
+	public ResponseEntity<String> chat(@RequestParam(value = "q", required = true) String q) {
+
+		var resultResponse=chatClient.prompt(q).call().content();
+		return ResponseEntity.ok(resultResponse);
+
+	}
+
+}
